f98599258acaab7c561f268e8fb32a13959725ca
danijels <73341306+danijels@users.noreply.github.com>
2021-01-27 11:53:26 +0100

    Updates from the challenges

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,168 +1,95 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-suite('Unit Tests', function () {
-  suite('Basic Assertions', function () {
+const server = require("../server");
+
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+
+suite("Functional Tests", function () {
+  suite("Integration tests with chai-http", function () {
     // #1
-    test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'this is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+    test("Test GET /hello with no name", function (done) {
+      chai
+        .request(server)
+        .get("/hello")
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello Guest");
+          done();
+        });
     });
     // #2
-    test('#isDefined, #isUndefined', function () {
-      assert.fail(null, 'null is not undefined');
-      assert.fail(undefined, 'undefined IS undefined');
-      assert.fail('hello', 'a string is not undefined');
+    test("Test GET /hello with your name", function (done) {
+      chai
+        .request(server)
+        .get("/hello?name=Danijela")
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello Danijela");
+          done();
+        });
     });
     // #3
-    test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey');
-      assert.fail("I'm truthy", 'a string is truthy');
-      assert.fail(true, 'true is truthy');
+    test('send {surname: "Colombo"}', function (done) {
+      chai
+        .request(server)
+        .put("/travellers")
+        .send({ surname: 'Colombo' })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
+          done();
+        });
     });
     // #4
-    test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'double negation of a truthy is true');
-      assert.fail({ value: 'truthy' }, 'A truthy object is NOT TRUE (neither is false...)');
+    test('send {surname: "da Verrazzano"}', function (done) {
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({ surname: 'da Verrazzano' })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
+          done();
+        });      
     });
   });
+});
 
-  // -----------------------------------------------------------------------------
+const Browser = require("zombie");
+Browser.site = "https://boilerplate-mochachai.danijels.repl.co";
 
-  suite('Equality', function () {
+suite("Functional Tests with Zombie.js", function () {
+  const browser = new Browser();
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
+
+  suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('#equal, #notEqual', function () {
-      assert.fail(12, '12', 'numbers are coerced into strings with == ');
-      assert.fail({ value: 1 }, { value: 1 }, '== compares object references');
-      assert.fail(6 * '2', '12', 'no more hints...');
-      assert.fail(6 + '2', '12', 'type your error message if you want');
+    test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
+      browser.fill("surname", "Colombo").pressButton("submit", function () {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Cristoforo');
+        browser.assert.text('span#surname', 'Colombo');
+        browser.assert.element('span#dates', 1);
+        done();
+      });
     });
     // #6
-    test('#strictEqual, #notStrictEqual', function () {
-      assert.fail(6, '6');
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * '2', 12);
-      assert.fail([1, 'a', {}], [1, 'a', {}]);
-    });
-    // #7
-    test('#deepEqual, #notDeepEqual', function () {
-      assert.fail({ a: '1', b: 5 }, { b: 5, a: '1' }, "keys order doesn't matter");
-      assert.fail({ a: [5, 6] }, { a: [6, 5] }, "array elements position does matter !!");
+    test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
+      browser.fill('surname', 'Vespucci').pressButton('submit', () => {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.element('span#dates', 1);
+        done();
+      })
     });
   });
-
-  // -----------------------------------------------------------------------------
-
-  function weirdNumbers(delta) {
-    return (1 + delta - Math.random());
-  }
-
-  suite('Comparisons', function () {
-    // #8
-    test('#isAbove, #isAtMost', function () {
-      assert.fail('hello'.length, 5);
-      assert.fail(1, 0);
-      assert.fail(Math.PI, 3);
-      assert.fail(1 - Math.random(), 1);
-    });
-    // #9
-    test('#isBelow, #isAtLeast', function () {
-      assert.fail('world'.length, 5);
-      assert.fail(2 * Math.random(), 0);
-      assert.fail(5 % 2, 2);
-      assert.fail(2 / 3, 1);
-    });
-    // #10
-    test('#approximately', function () {
-      assert.fail(weirdNumbers(0.5), 1, 0);
-      assert.fail(weirdNumbers(0.2), 1, 0);
-    });
-  });
-
-  // -----------------------------------------------------------------------------
-
-  const winterMonths = ['dec,', 'jan', 'feb', 'mar'];
-  const backendLanguages = ['php', 'python', 'javascript', 'ruby', 'asp'];
-  suite('Arrays', function () {
-    // #11
-    test('#isArray, #isNotArray', function () {
-      assert.fail('isThisAnArray?'.split(''), 'String.prototype.split() returns an Array');
-      assert.fail([1, 2, 3].indexOf(2), 'indexOf returns a number.');
-    });
-    // #12
-    test('Array #include, #notInclude', function () {
-      assert.fail(winterMonths, 'jul', "It's summer in july...");
-      assert.fail(backendLanguages, 'javascript', 'JS is a backend language !!');
-    });
-  });
-
-  // -----------------------------------------------------------------------------
-
-  const formatPeople = function (name, age) {
-    return '# name: ' + name + ', age: ' + age + '\n';
-  };
-  suite('Strings', function () {
-    // #13
-    test('#isString, #isNotString', function () {
-      assert.fail(Math.sin(Math.PI / 4), 'a float is not a string');
-      assert.fail(process.env.PATH, 'env vars are strings (or undefined)');
-      assert.fail(JSON.stringify({ type: 'object' }), 'a JSON is a string');
-    });
-    // #14
-    test('String #include, #notInclude', function () {
-      assert.fail('Arrow', 'row', "Arrow contains row...");
-      assert.fail('dart', 'queue', "But a dart doesn't contain a queue");
-    });
-    // #15
-    test('#match, #notMatch', function () {
-      const regex = /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
-      assert.fail(formatPeople('John Doe', 35), regex);
-      assert.fail(formatPeople('Paul Smith III', 'twenty-four'), regex);
-    });
-  });
-
-  // ----------------------------------------------------------------------------- 
-
-  const Car = function () {
-    this.model = 'cedan';
-    this.engines = 1;
-    this.wheels = 4;
-  };
-
-  const Plane = function () {
-    this.model = '737';
-    this.engines = ['left', 'right'];
-    this.wheels = 6;
-    this.wings = 2;
-  };
-
-  const myCar = new Car();
-  const airlinePlane = new Plane();
-
-  suite('Objects', function () {
-    // #16
-    test('#property, #notProperty', function () {
-      assert.fail(myCar, 'wings', 'A car has not wings');
-      assert.fail(airlinePlane, 'engines', 'planes have engines');
-      assert.fail(myCar, 'wheels', 'Cars have wheels');
-    });
-    // #17
-    test('#typeOf, #notTypeOf', function () {
-      assert.fail(myCar, 'object');
-      assert.fail(myCar.model, 'string');
-      assert.fail(airlinePlane.wings, 'string');
-      assert.fail(airlinePlane.engines, 'array');
-      assert.fail(myCar.wheels, 'number');
-    });
-    // #18
-    test('#instanceOf, #notInstanceOf', function () {
-      assert.fail(myCar, Plane);
-      assert.fail(airlinePlane, Plane);
-      assert.fail(airlinePlane, Object, 'everything is an Object');
-      assert.fail(myCar.wheels, String);
-    });
-  });
-
-  // -----------------------------------------------------------------------------
 });
